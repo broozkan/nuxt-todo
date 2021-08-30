01754f3b565d47c4d2fb7d44556e0b1339a1dccb
Burhan Özkan <burhan.ozkan@trhqmac121.local>
2021-08-30 17:00:55 +0300

    Added new test cases

--- test/FormTodo.spec.js
+++ test/FormTodo.spec.js
@@ -1,24 +1,18 @@
-import { mount } from '@vue/test-utils';
-import FormAddTodo from '../components/FormAddTodo.vue';
+import { mount } from '@vue/test-utils'
+import FormTodo from '../components/FormTodo.vue'
 
 
-describe('Form Add Todo tests', () => {
-
-    const wrapper = mount(FormAddTodo)
-    it('should todo title input exist and defined properly', () => {
-        const input = wrapper.get("[data-testid='title']")
-        expect(input.attributes('name')).toBe('title')
-        expect(input).toBeTruthy()
-    })
-
-    it('should exist is done checkbox', () => {
-        const checkbox = wrapper.get("[data-testid='is_done']")
-        expect(checkbox.attributes('name')).toBe('is_done')
-        expect(checkbox).toBeTruthy()
-    })
-
-    it('should exist submit button', () => {
-        const submitButton = wrapper.get("button[type='submit']")
-        expect(submitButton).toBeTruthy()
+describe('Form Todo Tests', () => {
+    it('should exist and gets the props', () => {
+        const formInıtialData = {
+            todo: {
+                title: '',
+                is_done: false
+            }
+        }
+        const wrapper = mount(FormTodo, {
+            propsData: formInıtialData
+        })
+        expect(wrapper.exists()).toBe(true)
     })
 })
--- test/Todo.spec.js
+++ test/Todo.spec.js
@@ -3,14 +3,17 @@ import Todo from '../components/Todo.vue'
 
 
 describe('Todo component tests', () => {
-    it('Is component takes the props', () => {
-        const propsData = {
+    it('Is component takes the props properly', () => {
+        const tododata = {
+            id: 1,
             title: 'test title',
             is_done: true
         }
-        const wrapper = mount(Todo, { propsData })
+        const wrapper = mount(Todo, {
+            propsData: { todo: tododata }
+        })
 
-        expect(wrapper.html()).toContain(propsData.title)
+        expect(wrapper.html()).toContain(tododata.title)
         expect(wrapper.html()).toContain('alert-success')
 
 
