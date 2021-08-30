8bcdcc2a9b44e8044fd91cc6648fc8e3bbed88bb
Burhan Özkan <burhan.ozkan@trhqmac121.local>
2021-08-30 17:01:21 +0300

    connected with api service

--- store/index.js
+++ store/index.js
@@ -1,42 +1,59 @@
+import apiService from "../services/api.service";
+
 export const state = () => ({
-    todos: [
-        { "id": 1, "title": "HTML I", "is_done": true },
-        { "id": 2, "title": "CSS", "is_done": true },
-        { "id": 3, "title": "Responsive design", "is_done": true },
-        { "id": 4, "title": "Git", "is_done": true },
-        { "id": 5, "title": "JavaScript I", "is_done": true },
-        { "id": 6, "title": "JavaScript II", "is_done": false }
-    ]
+    todos: []
 })
-
-
 
 export const mutations = {
     ADD_TODO: (state, todo) => {
-        console.log(todo);
         state.todos.push(todo)
     },
     SET_TODOS: (state, todos) => {
-        //state.todos = todos
+        state.todos = todos
     },
     SET_TODO: (state, todo) => {
         state.todo = Object.assign({}, state.todo, todo)
     },
+    DELETE_TODO: (state, id) => {
+        const index = state.todos.findIndex((todo) => {
+            todo.id === id
+        })
+        state.todos.splice(index, 1)
+    },
     UPDATE_TODO: (state, todo) => {
-        console.log(
-            todo
-        );
+        const index = state.todos.findIndex((todoMap) => {
+            todoMap.id === todo.id
+        })
+        state.todos[index] = todo
     }
-
 }
 
 
 export const actions = {
     addTodo({ commit }, todo) {
-        return commit('ADD_TODO', todo)
+        return apiService.addTodo(todo).then(() => {
+            commit('ADD_TODO', todo)
+        })
     },
     putTodo({ commit }, todo) {
-        return commit('UPDATE_TODO', todo)
+        return apiService.putTodo(todo).then(() => {
+            commit('UPDATE_TODO', todo)
+        })
+    },
+    deleteTodoById({ commit }, id) {
+        return apiService.deleteTodo(id).then((response) => {
+            return commit('DELETE_TODO', id)
+        })
+    },
+    fetchTodos({ commit }) {
+        return apiService.getTodos().then((response) => {
+            commit('SET_TODOS', response.data)
+        })
+    },
+    fetchTodo({ commit }, id) {
+        return apiService.getTodo(id).then((response) => {
+            commit('SET_TODO', response.data)
+        })
     }
 
 }
